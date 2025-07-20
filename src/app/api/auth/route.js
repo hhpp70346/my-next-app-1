527221a90d4ceda4a4a3e246cb939fd3e9e91920
hhpp70346 <hhpp70346@Gmail.com>
2025-07-20 03:46:40 +0300

    Update route.js

--- src/app/api/auth/route.js
+++ src/app/api/auth/route.js
@@ -4,11 +4,10 @@ import { NextResponse } from 'next/server';
 
 // تكوين اتصال قاعدة البيانات
 const dbConfig = {
-  port: "21",
-  host: "ftpupload.net",
-  user: "if0_38114427",
-  password: "hhhhhsssss12345",
-  database: "if0_38114427_a1"
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "A1"
 };
 
 export async function POST(request) {
